641f5379eb7a72f9d397525577f7748d03ab5645
xokc4 <42719177+xokc4@users.noreply.github.com>
2021-06-06 19:44:55 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace VosrahSlova
+{
+    class Program
+    { 
+        public static string RemoveConsecDuplicates(string s) => Regex.Replace(s, @"(.)\1+", "$1");// заменяет строки
+
+
+    
+    
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();//ввод текста
+            VosratSlova(text);//вызов метода
+            Console.ReadKey();
+
+
+        }
+        static void VosratSlova(string text)
+        {
+            string text2 = text.ToLower();//изменения текста под маленький шрифт
+
+
+            Console.WriteLine(RemoveConsecDuplicates(text2));//вывод нового текста
+
+        }
+    }
+}
